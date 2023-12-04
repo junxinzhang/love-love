863d7e8f1d38791a0fe076c58fd70b5450a64ed8
junxin.zhang <junxin.zhang@mathartsys.com>
2023-12-04 17:14:22 +0800

    fix deploy cf-pages error https://github.com/enricoros/big-agi/issues/72

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        // defaults to TRUE, unless API Keys are set at build time; this flag is used by the UI
+        REQUIRE_USER_API_KEYS: !process.env.OPENAI_API_KEY,
+    },
+    experimental: {
+        runtime: 'edge',
+    },
+    webpack(config, { isServer, dev }) {
+        // @mui/joy: anything material gets redirected to Joy
+        config.resolve.alias['@mui/material'] = '@mui/joy';
 
-module.exports = nextConfig
+        // @dqbd/tiktoken: enable asynchronous WebAssembly
+        config.experiments = {
+            asyncWebAssembly: true,
+            layers: true,
+        };
+
+        return config;
+    },
+};
+
+module.exports = nextConfig;
